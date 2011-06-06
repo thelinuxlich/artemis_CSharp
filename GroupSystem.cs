8256bfc1bf9df6d361dc7ef25f49d1de4ce722b4
thelinuxlich <thelinuxlich@gmail.com>
2011-06-06 20:30:26 -0300

    Adding TagSystem and GroupSystem

---
+++ GroupSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Artemis
+{
+    public abstract class GroupSystem : EntitySystem
+    {
+        private String group;
+        public GroupSystem(String group) {
+            this.group = group;
+		}
+		
+		/**
+		 * Process a entity this system is interested in.
+		 * @param e the entity to process.
+		 */
+		public abstract void Process(Entity e);
+
+        public override void ProcessEntities(Dictionary<int, Entity> entities)
+        {
+            Bag<Entity> groupedEntities = this.world.GetGroupManager().getEntities(group);
+            for (int i = 0, j = groupedEntities.Size(); i < j; i++)            {
+                Process(groupedEntities.Get(i));
+            } 
+		}
+		
+		public override bool CheckProcessing() {
+			return true;
+		}
+    }
+}
---
+++ TagSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Artemis
+{
+    public abstract class TagSystem : EntitySystem
+    {
+        private String tag;
+        public TagSystem(String tag) {
+            this.tag = tag;
+		}
+		
+		/**
+		 * Process a entity this system is interested in.
+		 * @param e the entity to process.
+		 */
+		public abstract void Process(Entity e);
+
+        public override void ProcessEntities(Dictionary<int, Entity> entities)
+        {
+            Entity e = this.world.GetTagManager().GetEntity(tag);
+            Process(e);
+		}
+		
+		public override bool CheckProcessing() {
+			return true;
+		}
+    }
+}
