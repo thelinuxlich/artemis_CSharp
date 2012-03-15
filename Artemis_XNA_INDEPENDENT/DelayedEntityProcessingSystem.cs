d4a86f161b8436b18c4511aa15e15e0faf14e996
capnjosh1 <clausen@sosproduce.com>
2012-03-14 20:47:40 -0700

    Added Intellisense summaries to DelayedEntityProcessingSystem.cs for both XNA_Independent as well as XBox_Phone7 projects

--- Artemis_XNA_INDEPENDENT/DelayedEntityProcessingSystem.cs
+++ Artemis_XNA_INDEPENDENT/DelayedEntityProcessingSystem.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 namespace Artemis
 {
 	public abstract class DelayedEntityProcessingSystem : DelayedEntitySystem {
-	
-		/**
-		 * Create a new DelayedEntityProcessingSystem. It requires at least one component.
-		 * @param requiredType the required component type.
-		 * @param otherTypes other component types.
-		 */
+
+		
+        /// <summary>
+        /// Create a new DelayedEntityProcessingSystem. It requires at least one component.
+        /// </summary>
+        /// <param name="requiredType">The required component type.</param>
+        /// <param name="otherTypes">Other component types.</param>
 		public DelayedEntityProcessingSystem(Type requiredType,params Type[] otherTypes) : base(GetMergedTypes(requiredType, otherTypes)){
 		}
-		
-		/**
-		 * Process a entity this system is interested in.
-		 * @param e the entity to process.
-		 */
+
+        /// <summary>
+        /// Process an entity this system is interested in.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="accumulatedDelta">The entity to process.</param>
 		public abstract void Process(Entity e, int accumulatedDelta);
 
+
+        /// <summary>
+        /// Process all entities with the delayed Entity processing system
+        /// </summary>
+        /// <param name="entities">Entities to process</param>
+        /// <param name="accumulatedDelta">Total Delay</param>
         public override void ProcessEntities(Dictionary<int, Entity> entities, int accumulatedDelta)
         {
 			foreach (Entity item in entities.Values)
