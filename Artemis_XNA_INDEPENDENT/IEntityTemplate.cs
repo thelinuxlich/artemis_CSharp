a687188d036cc87a683bff1fd3f082c39bbb86ac
thelinuxlich <thelinuxlich@thelinuxlich.(none)>
2012-08-08 16:19:15 -0300

    Adding missing files

---
+++ Artemis_XNA_INDEPENDENT/IEntityTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Artemis
+{
+    public interface IEntityTemplate
+    {
+        Entity BuildEntity(Entity e);
+    }
+}
