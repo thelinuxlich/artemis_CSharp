aeaa90b662ffa0e951ab13b815f1a5159107246e
GrafSeismo <graf.seismo@googlemail.com>
2013-02-26 12:39:53 +0100

    Project clean up
    General Project clean up,
    Add post build script,
    Fix release build error,
    Reorganised unit tests (VS2010 only left, convert to 2012 still
    possible, but pls add changed files separate).

---
+++ Artemis_UnitTests/PerformanceTest.cs
@@ -0,0 +1,75 @@
+﻿#region File description
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PerformanceTest.cs" company="GAMADU.COM">
+//     Copyright © 2013 GAMADU.COM. All rights reserved.
+//
+//     Redistribution and use in source and binary forms, with or without modification, are
+//     permitted provided that the following conditions are met:
+//
+//        1. Redistributions of source code must retain the above copyright notice, this list of
+//           conditions and the following disclaimer.
+//
+//        2. Redistributions in binary form must reproduce the above copyright notice, this list
+//           of conditions and the following disclaimer in the documentation and/or other materials
+//           provided with the distribution.
+//
+//     THIS SOFTWARE IS PROVIDED BY GAMADU.COM 'AS IS' AND ANY EXPRESS OR IMPLIED
+//     WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//     FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GAMADU.COM OR
+//     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//     CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//     SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//     ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//     NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//     ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//     The views and conclusions contained in the software and documentation are those of the
+//     authors and should not be interpreted as representing official policies, either expressed
+//     or implied, of GAMADU.COM.
+// </copyright>
+// <summary>
+//   The performance tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion File description
+
+namespace UnitTests
+{
+    #region Using statements
+
+    using global::System;
+    using global::System.Diagnostics;
+
+    using Artemis.Utils;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    #endregion Using statements
+
+    /// <summary>The performance tests.</summary>
+    [TestClass]
+    public class PerformanceTest
+    {
+        /// <summary>The bag performance test a.</summary>
+        [TestMethod]
+        public void BagPerformanceTestA()
+        {
+            Bag<int> bigBag = new Bag<int>(16);
+            Stopwatch watch = Stopwatch.StartNew();
+            int x = 50;
+            for (int index = 0; index <= 1000000; ++index)
+            {
+                // Set it to a billion to get a OutOfMemoryException
+                // due to Bag Grow method generating too many items.
+                bigBag.Add(index + (x / 2) + 1);
+                ++x;
+            }
+
+            watch.Stop();
+
+            // It is fast.
+            Console.WriteLine(watch.Elapsed);
+        }
+    }
+}
---
+++ Artemis_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,75 @@
+﻿#region File description
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="GAMADU.COM">
+//     Copyright © 2013 GAMADU.COM. All rights reserved.
+//
+//     Redistribution and use in source and binary forms, with or without modification, are
+//     permitted provided that the following conditions are met:
+//
+//        1. Redistributions of source code must retain the above copyright notice, this list of
+//           conditions and the following disclaimer.
+//
+//        2. Redistributions in binary form must reproduce the above copyright notice, this list
+//           of conditions and the following disclaimer in the documentation and/or other materials
+//           provided with the distribution.
+//
+//     THIS SOFTWARE IS PROVIDED BY GAMADU.COM 'AS IS' AND ANY EXPRESS OR IMPLIED
+//     WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//     FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GAMADU.COM OR
+//     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//     CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//     SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//     ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//     NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//     ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//     The views and conclusions contained in the software and documentation are those of the
+//     authors and should not be interpreted as representing official policies, either expressed
+//     or implied, of GAMADU.COM.
+// </copyright>
+// <summary>
+//   The assembly information file.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion File description
+
+#region Using statements
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+#endregion Using statements
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("Unit tests for the Artemis Entity System.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GAMADU.COM")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © 2013 GAMADU.COM. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9b7b5f92-c660-49ca-80e3-b3615aa76361")]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
