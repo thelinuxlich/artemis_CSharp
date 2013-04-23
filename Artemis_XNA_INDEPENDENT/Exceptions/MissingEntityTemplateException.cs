a67c53d3455b1ffbcb04fd87c5519eb9175ca6b8
Markus <marwes91@gmail.com>
2013-04-23 12:16:18 +0200

    Added MissingTemplateException class
    Used when trying to create a entity from a template which does not exist

---
+++ Artemis_XNA_INDEPENDENT/Exceptions/MissingEntityTemplateException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Artemis.Exceptions
+{
+    /// <summary>
+    /// Exception that is thrown when trying to use a EntityTemplate which does not exist.
+    /// </summary>
+    [global::System.Serializable]
+    public class MissingEntityTemplateException : global::System.Exception
+    {
+        internal MissingEntityTemplateException(string entityTemplateTag)
+            : this(entityTemplateTag, null) { }
+        internal MissingEntityTemplateException(string entityTemplateTag, global::System.Exception inner)
+            : base("EntityTemplate for the tag " + entityTemplateTag + " was not registered.", inner) { }
+        protected MissingEntityTemplateException(
+          global::System.Runtime.Serialization.SerializationInfo info,
+          global::System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
