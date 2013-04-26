0b79b235e1b6134f1bb6076e8e7877d161187e9a
Markus <marwes91@gmail.com>
2013-04-26 19:02:52 +0200

    Added unittests for EntityComponentSystem<...> and IComponent<T>

---
+++ Artemis_UnitTests/Component/TestBaseComponent.cs
@@ -0,0 +1,18 @@
+﻿using Artemis.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.Component
+{
+    class TestBaseComponent : IComponent<TestBaseComponent>
+    {
+        public virtual bool IsDerived()
+        {
+            return false;
+        }
+
+        public int Value { get; set; }
+    }
+}
