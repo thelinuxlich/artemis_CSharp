dd052be873889549cd21ee9b97211a81ef17b76a
Thiago Dias Pastor <pastot@amazon.com>
2020-01-31 22:10:25 +0100

    cleanup and initial migration

---
+++ ArtemisTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpVSMacMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
